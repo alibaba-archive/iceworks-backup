7972e8132917b0680a50248bb8dbccfc765fdd61
wuji.xwt <wuji.xwt@alibaba-inc.com>
2020-07-28 17:47:30 +0800

    feat: close notice

--- tools/iceworks/renderer/src/components/Notice/index.jsx
+++ tools/iceworks/renderer/src/components/Notice/index.jsx
@@ -6,18 +6,22 @@ class Notice extends Component {
   state = {
     hidden: false,
   };
+
   handleClose = () => {
     this.setState({
-      hidden: true 
+      hidden: true,
     });
   }
+
   render() {
-    return !this.state.hidden && <div className="component-notice">
+    return !this.state.hidden && (
+    <div className="component-notice">
       <a href="https://ice.work/iceworks">
         Iceworks 客户端已停止维护，请使用最新的 Iceworks 套件 >>>
       </a>
       <Icon type="close" size="xs" onClick={this.handleClose} />
-    </div>;
+    </div>
+    );
   }
 }
 
